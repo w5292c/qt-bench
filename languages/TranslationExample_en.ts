90be2d7ecc7784b1d138f37776402c4ea5f4d618
Alexander Chumakov <w5292c@gmail.com>
2022-06-23 17:50:23 +0300

    Updated linenumbers in localization files

--- languages/TranslationExample_en.ts
+++ languages/TranslationExample_en.ts
@@ -4,16 +4,16 @@
 <context>
     <name></name>
     <message id="example-id-without-params">
-        <location filename="../src/main.cpp" line="67"/>
+        <location filename="../src/main.cpp" line="63"/>
         <source>Simple localization example without parameters</source>
         <extracomment>This is a localization example without parameters</extracomment>
         <translation>Simple localization example without parameters</translation>
         <extra-Context>Main function</extra-Context>
     </message>
     <message id="example-id-number-of-files" numerus="yes">
-        <location filename="../src/main.cpp" line="75"/>
-        <location filename="../src/main.cpp" line="76"/>
-        <location filename="../src/main.cpp" line="77"/>
+        <location filename="../src/main.cpp" line="71"/>
+        <location filename="../src/main.cpp" line="72"/>
+        <location filename="../src/main.cpp" line="73"/>
         <source>Number of files: %n</source>
         <extracomment>Reports the number of files to the console</extracomment>
         <translation>
